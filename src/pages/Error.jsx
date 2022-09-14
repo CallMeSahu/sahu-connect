faf7ea40799075b6f8946653b4b1f9821b227890
Siddhartha <siddharthasahu.work@gmail.com>
2022-09-15 03:13:51 +0530

    feat: error

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+import error from "../../src/assets/error.svg";
 
-
-const Error = () => {
-  
+export function Error(){
+  const navigate = useNavigate();
   return (
-    <div></div>
+    <div className="bg-slate-100 flex flex-col items-center justify-center errorbox">
+      <img src={error} alt="" className="h-2/3" />
+      <div>
+        <button
+          className="w-full mt-4 rounded-lg bg-blue-500 hover:bg-blue-600 text-white font-bold px-7 py-2"
+          onClick={() => navigate("/login")}
+        >
+          Go Back to Login
+        </button>
+      </div>
+    </div>
   );
-};
-
-export { Error };
+}
