7e9ae54d4d3fdc53975d213c7f9cb0a1bd82d571
Siddhartha <siddharthasahu.work@gmail.com>
2022-09-15 02:36:48 +0530

    feat: profile pages

--- src/pages/profile/components/EditProfileModal.jsx
+++ src/pages/profile/components/EditProfileModal.jsx
@@ -1,11 +1,132 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { closeEditProfileModal } from "../profileModalSlice";
+import { updateUserInfo } from "../userSlice";
 
+export function EditProfileModal(){
+  const { editProfileModal } = useSelector((state) => state.profileModal);
+  const { token } = useSelector((state) => state.auth);
+  const { authUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const [updateUserData, setUpdateUserData] = useState({});
+  const [loading, setLoading] = useState(false);
 
-const EditProfileModal = () => {
-  
+  const updateUserInforHandler = () => {
+    dispatch(updateUserInfo({ userData: { ...updateUserData }, token: token }));
+    dispatch(closeEditProfileModal());
+    toast.success("Profile Updated!");
+  };
 
-  return (
-    <div></div>
-  );
-};
+  useEffect(() => {
+    setUpdateUserData({ ...authUser });
+  }, [authUser]);
 
-export { EditProfileModal };
+  const updateImageHandler = async (e) => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+    data.append("upload_preset", "sls1eclu");
+    setLoading(true);
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/dus3r5adq/image/upload",
+      { method: "POST", body: data }
+    );
+    const file = await res.json();
+    setUpdateUserData({ ...updateImageHandler, avatar: file.secure_url });
+    setLoading(false);
+  };
+  return(
+    <div
+      className={`container fixed justify-center items-center ${
+        editProfileModal ? "flex" : "hidden"
+      }`}
+    >
+      <div className="w-1/3 bg-white p-3 flex flex-col rounded-lg sm:w-full md:w-3/4">
+        <div className="flex items-center justify-between ">
+          <h2 className="text-lg md:text-md font-bold">Edit Profile</h2>
+          <button
+            className=" border-blue-500 px-1.5"
+            onClick={() => {
+              dispatch(closeEditProfileModal());
+            }}
+          >
+            <i className="bx bx-x text-lg"></i>
+          </button>
+        </div>
+        <div className="flex flex-col mt-3">
+          <div className="flex justify-between gap-4 mb-3">
+            <div className="gap-2 text-slate-500 text-md md:text-md">
+              Avatar:
+            </div>
+              <div className="w-9/12 relative">
+                <img
+                  src={updateUserData?.avatar}
+                  alt="user_image"
+                  className="h-20 w-20 object-cover rounded-full sm:h-10 sm:w-10 "
+                />
+                <i className="text-slate-800 bx bxs-camera absolute top-12 left-14 text-lg cursor-pointer bg-white rounded-full px-1 border"></i>
+                <input
+                  type="file"
+                  className="absolute opacity-0 w-8  top-7 left-7 cursor-pointer"
+                  accept="image/jpeg, image/png, image/svg+xml, image/jpg, image/webp, image/apng, image/avif, image/gif,"
+                  onChange={updateImageHandler}
+                />
+              </div>
+          </div>
+          <div className="flex justify-between gap-4 mb-3">
+            <div className="gap-2 text-slate-500 text-md md:text-md">
+              FirstName:
+            </div>
+            <input
+              value={updateUserData.firstName}
+              onChange={(e) =>
+                setUpdateUserData({
+                  ...updateUserData,
+                  firstName: e.target.value,
+                })
+              }
+              className="border focus:outline-none py-2 px-3 w-9/12 rounded-md text-sm"
+            />
+          </div>
+          <div className="flex justify-between gap-4 mb-3">
+            <div className="gap-2 text-slate-500 text-md md:text-md">
+              LastName:
+            </div>
+            <input
+              value={updateUserData.lastName}
+              onChange={(e) =>
+                setUpdateUserData({
+                  ...updateUserData,
+                  lastName: e.target.value,
+                })
+              }
+              className="border focus:outline-none py-2 px-3 w-9/12 rounded-md text-sm"
+            />
+          </div>
+          <div className="flex justify-between gap-4 mb-3">
+            <div className="gap-2 text-slate-500 text-md md:text-md">Bio:</div>
+            <input
+              value={updateUserData.bio}
+              onChange={(e) =>
+                setUpdateUserData({ ...updateUserData, bio: e.target.value })
+              }
+              className="border focus:outline-none py-2 px-3 w-9/12 rounded-md text-sm"
+            />
+          </div>
+          <div className="flex justify-end">
+            <button
+              className={
+                "w-full mt-4 rounded-lg bg-blue-500 hover:bg-blue-600 text-white py-2"
+              }
+              onClick={() => updateUserInforHandler()}
+            >
+              Update
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
--- src/pages/profile/components/IndividualUserProfile.jsx
+++ src/pages/profile/components/IndividualUserProfile.jsx
@@ -1,9 +1,98 @@
+import { SinglePost } from "../../home/components/SinglePost";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { followUserHandler, unfollowUser } from "../../authentication/authSlice";
+import { getUserPost } from "../../home/postSlice";
+import { getUsers } from "../userSlice";
 
-const IndividualUserProfile = () => {
-  
-  return (
-    <div></div>
+export function IndividualUserProfile(){
+ const { userPosts, allPosts } = useSelector((state) => state.post);
+  const dispatch = useDispatch();
+  const userId = useParams();
+  const { user, token } = useSelector((state) => state.auth);
+  const { allUsers } = useSelector((state) => state.user);
+  const [individualUser, setIndividualUser] = useState({});
+
+  useEffect(() => {
+    setIndividualUser(allUsers.find((item) => item.username === userId.userId));
+  }, [allUsers, userId]);
+
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [allUsers]);
+
+  useEffect(() => {
+    dispatch(getUserPost(individualUser?._id));
+  }, [individualUser, allPosts]);
+
+  const isFollowing = individualUser?.followers?.some(
+    (item) => item.username === user.username
   );
-};
 
-export { IndividualUserProfile };
+  console.log(isFollowing);
+  const followUser = (_id) => {
+    if (isFollowing) {
+      dispatch(unfollowUser({ followUserId: _id, token: token }));
+    } else {
+      dispatch(followUserHandler({ followUserId: _id, token: token }));
+    }
+  };
+
+  console.log(individualUser); 
+
+  return(
+    <div className="w-5/6 xl:w-full px-1 mb-8">
+      <div className="w-full" key={individualUser._id}>
+          <div className="bg-slate-100  flex flex-col px-2 py-2 rounded-md  w-full my-2">
+            <div className="flex flex-col items-center">
+              <div className="flex justify-between items-center h-20 w-20 rounded-full">
+                <img
+                  src={individualUser?.avatar}
+                  alt="avatar"
+                  className="h-20 w-20 object-cover rounded-full sm:h-16 sm:w-16 border-0"
+                />
+              </div>
+
+              <span className="text-xl font-semibold mt-2">
+                {`${individualUser.firstName} ${individualUser.lastName}`}
+              </span>
+              <span className="text-sm text-gray-500">
+                @{individualUser.username}
+              </span>
+
+              <div className="font-semibold text-gray-500 mt-3">
+                {individualUser.bio}
+              </div>
+
+              <div className="font-semibold text-gray-500 mt-1.5 flex gap-5">
+                <span>{userPosts?.length} Posts</span>
+                <span>
+                  {individualUser?.followers?.length > 0
+                    ? individualUser?.followers?.length
+                    : 0}{" "}
+                  Followers
+                </span>
+                <span className="cursor-pointer border-b-5">
+                  {individualUser?.following?.length > 0
+                    ? individualUser?.following?.length
+                    : 0}{" "}
+                  Following
+                </span>
+              </div>
+
+              <button
+                onClick={() => followUser(individualUser?._id)}
+                className="mt-4 border rounded-md px-6 py-1.5 bg-blue-500 text-white hover:bg-blue-600 cursor-pointer"
+              >
+                {!isFollowing ? "Follow" : "Unfollow"}
+              </button>              
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+
